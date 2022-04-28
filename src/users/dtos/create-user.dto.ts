f0d0086b0d417b881cbc338a03935f5e04770e90
migueloliveiraz <migueloliveiradev@gmail.com>
2022-04-28 09:18:25 -0300

    refactor: add validation in body fields

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  surname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  age: number;
+
+  @IsString()
+  avatar: string;
+}
